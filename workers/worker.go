2de1a36d2326f2b78954a42a7bac551999e8d68a
acuscorp <57365344+acuscorp@users.noreply.github.com>
2022-09-13 16:26:12 +0000

    workers in parallel receiving urls not sequentially

--- workers/worker.go
+++ workers/worker.go
@@ -2,43 +2,44 @@ package workers
 
 import "fmt"
 
-
-func getPage(url string) (int, error){
-  return len(url), nil
+func getPage(url string) (int, error) {
+	return len(url), nil
 }
 func worker(urlCh chan string, sizeCh chan string, id int) {
-  for{
-    url := <- urlCh
-    length, err := getPage(url)
-    if err == nil {
-      sizeCh <- fmt.Sprintf("%s has length %d, id: (%d)", url, length, id)
-    } else {
-      sizeCh <- fmt.Sprintf("Error getting %s: %s, id:(%d)", url, err, id)
-    }
-  }
+	for {
+		url := <-urlCh
+		length, err := getPage(url)
+		if err == nil {
+			sizeCh <- fmt.Sprintf("%s has length %d, id: (%d)", url, length, id)
+		} else {
+			sizeCh <- fmt.Sprintf("Error getting %s: %s, id:(%d)", url, err, id)
+		}
+	}
+}
+func generator(url string, urlCh chan string) {
+	urlCh <- url
 }
 
 func Initialize() {
-  urls := []string{
-    "http://www.google.com.com/",
-    "http://www.facebook.com.com/",
-    "http://www.twitter.com.com/",
-    "http://www.instagram.com.com/",
-  }
+	urls := []string{
+		"http://www.google.com.com/",
+		"http://www.facebook.com.com/",
+		"http://www.twitter.com.com/",
+		"http://www.instagram.com.com/",
+	}
 
-  sizeCh := make(chan string) 
-  urlCh := make(chan string) 
+	sizeCh := make(chan string)
+	urlCh := make(chan string)
 
-  for id := 0; id < 10; id++ {
-    go worker(urlCh,sizeCh, id)
-  }
+	for id := 0; id < 10; id++ {
+		go worker(urlCh, sizeCh, id)
+	}
 
-  for _,url := range urls {
-    urlCh <- url  
-  }
-  for i:= 0; i < len(urls); i++ {
-    fmt.Printf("%s\n", <-sizeCh)
-  }
-  
-  
+	for _, url := range urls {
+		go generator(url, urlCh)
+	}
+	for i := 0; i < len(urls); i++ {
+		fmt.Printf("%s\n", <-sizeCh)
+	}
+
 }
