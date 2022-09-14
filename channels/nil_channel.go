9e381188fb083b905c51a798116c5405096de157
acuscorp <57365344+acuscorp@users.noreply.github.com>
2022-09-14 14:40:43 +0000

    stopper on reader and writer using nil channels using time.NewTimer

--- channels/nil_channel.go
+++ channels/nil_channel.go
@@ -14,19 +14,31 @@ func InitializeNilChannel(){
 }
 
 func Reader(ch chan int) {
-  t := time.NewTicker(3 * time.Second)
+  t := time.NewTimer(3 * time.Second)
   for {
     select{
       case n := <- ch:
         fmt.Println(n)
       case <- t.C:
         ch = nil
+        fmt.Println("Stopped on reader")
     }
   }
 }
 
 func Writer(ch chan int){
+  stopper := time.NewTimer(1 * time.Second)
+  restarter := time.NewTimer(2 * time.Second)
+  backupCh := ch
   for {
-    ch <- rand.Intn(40)
+    select{
+      case ch <- rand.Intn(40):
+      case <- stopper.C:
+        ch = nil       
+        fmt.Println("Stopped on writer")
+      case  <- restarter.C:
+        ch = backupCh
+        fmt.Println("restarted on writer")
+    }
   }
 }
