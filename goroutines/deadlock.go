b473ad9c73017e0045d78c91aa3f79478f13fc31
Noe-AdrianAdria <>
2022-09-15 15:37:46 +0000

    getting a deadlock

---
+++ goroutines/deadlock.go
@@ -0,0 +1,17 @@
+package goroutines
+import "fmt"
+func InitDeadLock(){
+  ch1 := make(chan int)
+  ch2 := make(chan int)
+  go func(){
+    v := 1
+    ch1 <- v
+    v2 := <- ch2
+    fmt.Println(v,v2)
+  }()
+  v := 2
+  ch2 <- v
+  v2 := <- ch1
+  fmt.Println(v,v2)
+  
+}
--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
-
 import (
-  "fmt"
-	"main/poetry"
+  g "main/goroutines"
 )
-
 func main() {
-	p := poetry.Poem{{"It will be soon midsummer of a fleeting year,",
-		"The sward will be brown, flowers faint and few,",
-		"Songbirds are hushed all but a faint but clear song",
-		"And 'larum of the bird-boy reach the ear,",
-		"Through the warm air floats forth lime's sweet scent,",
-		"And wayside branches have lost the rose's bloom.",
-		"The corn is golden along a thousand sea like slopes"},
-	}
-	v, c := p.Stats()
-  fmt.Printf("Vowels: %d, Consonants: %d\n",v, c)
-  fmt.Printf("Stanzas: %d Lines: %d\n", p.NumStanzas(),p.NumLines() )
+  g.InitDeadLock()
 }
