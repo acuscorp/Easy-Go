11ee5c154f6e87bf83f38f52a128900eceefb52d
noe.acuna <noe.acuna@solera.com>
2022-10-10 16:15:07 -0500

    web-service-gin example

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	g "main/generics"
+	g "main/typos"
 )
 
 func main() {
-	g.InitGenerics()
+	a := g.AType{}
+	a.SetName("noe")
+
+	b := a.GetB()     //AType has acces to BType
+	b.V = a.GetName() // now you can work with BType, But what happend if
+	g.InitTypos(b)    // you can't get values from variable
 }
